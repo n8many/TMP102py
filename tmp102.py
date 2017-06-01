609933c9c51caf080bb540387f4ee653102a8a94
n8many <n8nyoung@gmail.com>
2017-06-01 01:38:44 +0000

    Added check for valid register values

--- tmp102.py
+++ tmp102.py
@@ -1,25 +1,34 @@
 import smbus
 
+TEMPERTAURE_REG = 0x00
+CONFIG_REG = 0x01
+T_LOW_REG = 0x02
+T_HIGH_REG = 0x03
+
+ADDRESSES = [0x48, 0x49, 0x4A, 0x4B]
+
 class TMP102(object):
     def __init__(self, units=None, address=0x48, busnum=1):
         units = units or 'C'
+        if (address not in ADDRESSES):
+            raise ValueError("Invalid Address: {0:#x}".format(address))
         self.address = address
         self.busnum = busnum
-        
+
         self.setUnits(units)
         self.bus = smbus.SMBus(self.busnum)
         self.readSensor()
 
     def getUnits(self):
         return self.units
-    
+
     def setUnits(self, units):
         if (units.upper() in 'RCKF' and len(units) == 1):
             self.units = units.upper()
         else:
             raise ValueError("Invalid Unit, must use C(elcius), K(elvin),"
                     "F(ahrenheit), or R(ankine)")
-            
+
     def readSensor(self):
         data = self.bus.read_i2c_block_data(0x48, 0)
         return data
